fd43dc8215bb783f63861f5f0185d388d3c55c52
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-02-06 21:27:45 +0100

    Green

--- src/main/kotlin/com/example/kata/romannumerals/RomanNumeralConverter.kt
+++ src/main/kotlin/com/example/kata/romannumerals/RomanNumeralConverter.kt
@@ -2,7 +2,7 @@ package com.example.kata.romannumerals
 
 class RomanNumeralConverter {
     fun convert(arabic: Int): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return "I"
     }
 
 }
