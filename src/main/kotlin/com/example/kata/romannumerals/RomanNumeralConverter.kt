4049b5bce5e0a60a4569ca0dcc9c79e59aa7b0d5
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-02-06 21:27:47 +0100

    Green

--- src/main/kotlin/com/example/kata/romannumerals/RomanNumeralConverter.kt
+++ src/main/kotlin/com/example/kata/romannumerals/RomanNumeralConverter.kt
@@ -2,7 +2,16 @@ package com.example.kata.romannumerals
 
 class RomanNumeralConverter {
     fun convert(arabic: Int): String {
-        return "I"
+        return when (arabic) {
+            1 -> "I"
+            5 -> "V"
+            10 -> "X"
+            50 -> "L"
+            100 -> "C"
+            500 -> "D"
+            1000 -> "M"
+            else -> ""
+        }
     }
 
 }
