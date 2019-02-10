08be57851a600571f3c2218d782328894e38d89a
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-02-10 17:45:46 +0100

    Refactor: another way of expressing the same condition

--- src/main/kotlin/com/example/kata/romannumerals/ByDigitsConverter.kt
+++ src/main/kotlin/com/example/kata/romannumerals/ByDigitsConverter.kt
@@ -11,39 +11,44 @@ class ByDigitsConverter : RomanNumeralConverter {
 
             val currentDigit = fetchNextDigit(digits)
 
-            val x = currentDigit * Math.pow(10.0, digits.size.toDouble()).toInt()
+            val xx1 = digits.size.toDouble()
+            val magnitude = Math.pow(10.0, xx1).toInt()
+            val x = currentDigit * magnitude
 
+            val multiplesOfTen = listOf("I", "X", "C", "M")
             result += when (x) {
-                3000 -> "MMM"
-                2000 -> "MM"
-                1000 -> "M"
-                900 -> "CM"
+                3000, 300, 30, 3 -> {
+                    val selected = multiplesOfTen[xx1.toInt()]
+                    selected + selected + selected
+                }
+                2000, 200, 20, 2 -> {
+                    val selected = multiplesOfTen[xx1.toInt()]
+                    selected + selected
+                }
+                1000, 100, 10, 1 -> {
+                    val selected = multiplesOfTen[xx1.toInt()]
+                    selected
+                }
+                900, 90, 9 -> {
+                    val selectedSmall = multiplesOfTen[xx1.toInt()]
+                    val selectedBig = multiplesOfTen[xx1.toInt() + 1]
+                    selectedSmall + selectedBig
+                }
                 800 -> "DCCC"
                 700 -> "DCC"
                 600 -> "DC"
                 500 -> "D"
                 400 -> "CD"
-                300 -> "CCC"
-                200 -> "CC"
-                100 -> "C"
-                90 -> "XC"
                 80 -> "LXXX"
                 70 -> "LXX"
                 60 -> "LX"
                 50 -> "L"
                 40 -> "XL"
-                30 -> "XXX"
-                20 -> "XX"
-                10 -> "X"
-                9 -> "IX"
                 8 -> "VIII"
                 7 -> "VII"
                 6 -> "VI"
                 5 -> "V"
                 4 -> "IV"
-                3 -> "III"
-                2 -> "II"
-                1 -> "I"
                 else -> ""
             }
         }
